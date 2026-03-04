ffa6bce5694ad8b38c17de8191d0957705b01975
jaydengonzalezgu347-hue <jayden.gonzalezgu347@students.cmcss.net>
2026-03-04 10:14:34 -0600

    t

--- task3.py
+++ task3.py
@@ -1,6 +1,6 @@
-guard_awake = True
+guard_awake = False
 if guard_awake:
-    outcome = "Shadow: SNEAK PAST"
-else:
     outcome = "Doom: YOU HAVE BEEN CAUGHT"
+else:
+    outcome = "Shadow: SNEAK PAST"
 print(outcome)
